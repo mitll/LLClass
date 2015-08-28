683e3e03b0c7587022278dcbfc35e17595142612
Jennifer Williams <jennifer.williams@ll.mit.edu>
2015-08-28 19:04:59 -0400

    adds build script and MANIFEST

---
+++ mk.sh
@@ -0,0 +1,6 @@
+echo "compiling classydoc (Scala)"
+scalac -encoding UTF-8 -classpath lib/arg.jar:lib/avaje-ebeanorm-agent-3.1.1.jar:lib/commons-email-1.2.jar:lib/commons-io-2.4.jar:lib/commons-lang-2.5.jar:lib/jtr.jar:lib/libsvm.jar:lib/mail.jar:lib/mallet-deps.jar:lib/mallet.jar:lib/structlearn.jar src/*.scala
+echo "creating classydoc.jar"
+jar cvmf MANIFEST.MF classydoc.jar mitll/* lib/*
+echo "done."
+echo
