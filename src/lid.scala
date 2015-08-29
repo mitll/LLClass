81f41c5132395b9b25814120ebb3a5492b7ff490
Jennifer Williams <jennifer.williams@ll.mit.edu>
2015-08-29 10:14:42 -0400

    renames doc to lid, adds multiparam feature

--- src/lid.scala
+++ src/lid.scala
@@ -4,7 +4,40 @@ import utilities._
 import collection.mutable.{ArrayBuffer, ListBuffer, HashMap, Map};
 import text._
 
-object DOC extends InternalPipeRunner[Unit] with TrainerTemplate with ClassifierFactory[String] {
+
+ object multiparam {
+   
+   def sweepSVM (data : String) {
+     var coarseC = (-5 to 16) map { math.pow(2,_) }
+     var coarseGamma = (-5 to 6) map { math.pow(2,_) }
+     for (bkgmin <- Seq.range(2,21,1).par) { for (g <- coarseGamma.par) { for (C <- coarseC.par) {
+       var c = new LID;
+       var data_name = data.split(".")(0)
+       var title : String = "svm_sweep_results/" + data_name + "_" + bkgmin + "_" + g + "_" + C;
+       val params = "-train-method svm -all" + data + " -bkg-min-count " + bkgmin + " -split 0.20 -gamma " + g + " -c " + C + " -log " + title
+       val params2 : Array[String] = params.split(" ");
+       c.main(params2) }}}}
+   
+   def sweepMIRA (data : String) {
+    for (iterations <- Seq.range(300,3100,100).par) { for (bkgmin <- Seq.range(2,21,1).par) {           
+       for (slack <- Seq.iterate(0.0005,25)(0.00025+).par) {
+         var c = new LID;
+         var data_name = data.split(".")(0)
+         var title : String = "mira_sweep_results/" + data_name + "_" + iterations + "_" + slack + "_" + bkgmin //+ "_order-1"
+         val params = "-train-method mira -all" + data +" -iterations " + iterations + " -slack " + slack + " -bkg-min-count " + bkgmin + " -split 0.20 -log " + title
+         val params2 : Array[String] = params.split(" ");
+         c.main(params2) }}}}
+      
+   def main(args : Array[String]) {
+     var method = args(0);      
+     var data = args(1)
+     if (method == "sweep_svm") { sweepSVM(data) }
+     if (method == "sweep_mira") { sweepMIRA(data) }
+   }
+ }
+
+
+class LID extends InternalPipeRunner[Unit] with TrainerTemplate with ClassifierFactory[String] {
   // -------------------------------------------------------------------------------------------------------------------------------------
   // Data formats/readers
   // -------------------------------------------------------------------------------------------------------------------------------------
@@ -54,7 +87,7 @@ object DOC extends InternalPipeRunner[Unit] with TrainerTemplate with Classifier
   // Standalone runner, config and helpers
   // -------------------------------------------------------------------------------------------------------------------------------------
   val program = "doc-test";
-  var (wOrder, cOrder, minCount, prune, cutoff) = (1, 3, 2, 0.0f, 1e10f);
+  var (wOrder, cOrder, minCount, prune, cutoff) = (3, 3, 15, 0.0f, 1e10f);
 
   config += ("Front End") -> Params("bkg-min-count"      -> Arg(minCount _, minCount_= _, "Minimum count for background models"),
                                     "bkg-prune"          -> Arg(prune _, prune_= _,       "Prune features with p < this value from the background model"),
