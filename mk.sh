bce7b1150bded0db62c3c06f0655396925fce7b5
Jennifer Williams <jennifer.williams@ll.mit.edu>
2015-10-29 11:45:38 -0400

    removes files from old build. not needed for SBT build

--- mk.sh
+++
@@ -1,6 +0,0 @@
-echo "compiling classylid (Scala)"
-scalac -encoding UTF-8 -classpath lib/arg.jar:lib/avaje-ebeanorm-agent-3.1.1.jar:lib/commons-email-1.2.jar:lib/commons-io-2.4.jar:lib/commons-lang-2.5.jar:lib/jtr.jar:lib/libsvm.jar:lib/mail.jar:lib/mallet-deps.jar:lib/mallet.jar:lib/structlearn.jar src/*.scala
-echo "creating classylid.jar"
-jar cvmf MANIFEST.MF classylid.jar mitll/* lib/*
-echo "done."
-echo
