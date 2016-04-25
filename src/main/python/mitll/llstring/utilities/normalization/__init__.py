e051df9b959eea5f75c1aed3f91194ab7fa75b62
Gordon Vidaver <gordon.vidaver@ll.mit.edu>
2016-04-25 14:40:20 -0400

    removed python

--- src/main/python/mitll/llstring/utilities/normalization/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-The :mod:'llstring.normalization' sub-package implements
-generic and latin script text normalization. Included
-as well are functions for web and social media normalization.
-"""
-from .text_normalization import MITLLTextNormalizer
-from .latin_normalization import MITLLLatinNormalizer
-__all__ = ['MITLLTextNormalizer','MITLLLatinNormalizer']
